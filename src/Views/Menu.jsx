24afba031e8e96d5c44b636cd53833869b335c39
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-07-07 22:10:55 -0400

    images adjustment

--- src/Views/Menu.jsx
+++ src/Views/Menu.jsx
@@ -1,18 +1,23 @@
 import dishesData from '../assets/dishes.json'
 
 export default function Menu() {
+
+
   return (
     <section className='Menu'>
       <h1>Menu</h1>
       <p>Discover the vibrant essence of the Mediterranean at Little Lemon. While our roots are in cherished family recipes, we infuse each dish with a modern twist, bringing you the authentic tastes of the coast. Explore our collection where traditional flavors meet fresh, innovative interpretations. Come savor the best of the Mediterranean, reinvented.</p>
       <div className="dishes-grid">
-        {dishesData.map(dish => (
-          <article key={dish.id} className="dish-card">
+        {dishesData.map(dish => {
+          const imageUrl = import.meta.env.BASE_URL + dish.image.substring(1);
+          return (
+            <article key={dish.id} className="dish-card">
             <h3>{dish.name}</h3>
-            <img src={dish.image} alt={dish.name} />
+            <img src={imageUrl} alt={dish.name} />
             <p>{dish.info}</p>
           </article>
-        ))}
+          )
+        })}
       </div>
     </section>
   )
--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -6,12 +6,15 @@ export default function Dishes({ dishesData }) {
     <section className="dishes-section">
       <h2>Our Menu</h2>
       <div className="dishes-grid">
-        {dishesData.map(dish => (
-          <article key={dish.id} className="dish-card">
-            <h3>{dish.name}</h3>
-            <img src={dish.image} alt={dish.name} />
-          </article>
-        ))}
+        {dishesData.map(dish => {
+          const imageUrl = import.meta.env.BASE_URL + dish.image.substring(1);
+          return (
+            <article key={dish.id} className="dish-card">
+              <h3>{dish.name}</h3>
+              <img src={imageUrl} alt={dish.name} />
+            </article>
+          )
+        })}
       </div>
       <button className="menu-btn">
         <Link to="/Menu">Learn more about our menu</Link>
